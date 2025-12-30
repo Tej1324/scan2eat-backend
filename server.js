fc79c538fb20d55c282b2d42f68a0f2430c03f55
TEJA KODIYALA <tejakodiyala@MacBook-Air-von-TEJA.local>
2025-12-31 01:26:02 +0530

    Fix Express 5 CORS crash (remove app.options)

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 
 /* ================= MIDDLEWARE ================= */
 app.use(express.json({ limit: "100kb" }));
